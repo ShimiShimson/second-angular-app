d41adca721b2b85af87b267842198ce533d5db03
ShimiShimson <szymon.jakub.adamczuk@gmail.com>
2020-05-24 19:04:14 +0200

    "Another commit"

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -2,14 +2,19 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-servers',
-  template: `
-  <app-server></app-server>
-  <app-server></app-server>`,
+  // template: `
+  // <app-server></app-server>
+  // <app-server></app-server>`,
+  templateUrl: './servers.component.html',
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
-
-  constructor() { }
+  allowNewServer = false;
+  constructor() { 
+    setTimeout(() => {
+      this.allowNewServer = true;
+    } ,2000);
+  }
 
   ngOnInit(): void {
   }

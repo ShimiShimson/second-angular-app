b93592b46537655a0ceec4c22ef35b5374556a50
ShimiShimson <szymon.jakub.adamczuk@gmail.com>
2020-05-24 15:06:08 +0200

    Learning databinding

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -2,7 +2,6 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-servers',
-  // selector: '.app-servers',
   template: `
   <app-server></app-server>
   <app-server></app-server>`,

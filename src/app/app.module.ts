8cdd20572125cdb78fece170ff2dd881e44ff4ea
ShimiShimson <szymon.jakub.adamczuk@gmail.com>
2020-05-24 11:42:59 +0200

    "Second app from Angular Guide on Udemy"

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+// import { HttpModule } from '@angular/http';
+
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ServerComponent } from './server/server.component';
+import { ServersComponent } from './servers/servers.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ServerComponent,
+    ServersComponent
   ],
   imports: [
     BrowserModule,
+    FormsModule,
+    // HttpModule,
     AppRoutingModule
   ],
   providers: [],
